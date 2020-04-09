a5e9b5223cd1dcc2ed5071aab6f84c1b22b5ca30
joel@eyrie.in <joel@eyrie.in>
2020-04-09 06:42:07 +0530

    My changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,64 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  QuizBrain quizBrain = QuizBrain();
+
+  List<Widget> scoreKeeper = [];
+
+  int questionIndex = 0;
+
+  void addCorrectMark() {
+    scoreKeeper.add(Icon(
+      Icons.check,
+      color: Colors.green,
+    ));
+  }
+
+  void addWrongMark() {
+    scoreKeeper.add(Icon(
+      Icons.close,
+      color: Colors.red,
+    ));
+  }
+
+  void increment() {
+    print('Number of questions is ${quizBrain.totalQuestions()}');
+    questionIndex++;
+    if (questionIndex == quizBrain.totalQuestions()) {
+      questionIndex = 0;
+    }
+  }
+
+//  void answerClicked(bool choice) {
+//    print("Clicked $choice");
+//    if (quizBrain.getQuestionAnswer() == choice) {
+//      addCorrectMark();
+//    } else {
+//      addWrongMark();
+//    }
+//    quizBrain.nextQuestion();
+//  }
+
+  void trueClicked() {
+    print("Clicked True.");
+    if (quizBrain.getQuestionAnswer() == true) {
+      addCorrectMark();
+    } else {
+      addWrongMark();
+    }
+    quizBrain.nextQuestion();
+  }
+
+  void falseClicked() {
+    print("Clicked False.");
+    if (quizBrain.getQuestionAnswer() == false) {
+      addCorrectMark();
+    } else {
+      addWrongMark();
+    }
+    quizBrain.nextQuestion();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +96,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +120,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() {
+                  trueClicked();
+                });
                 //The user picked true.
               },
             ),
@@ -79,19 +141,20 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  falseClicked();
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Expanded(
+          child: Wrap(
+            direction: Axis.horizontal,
+            children: scoreKeeper,
+          ),
+        )
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
